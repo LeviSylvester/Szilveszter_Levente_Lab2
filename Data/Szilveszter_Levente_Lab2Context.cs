9db93e97c95df88f9a71afcd57467fcb9e379775
Levi Szilveszter <l3vi.sylvester@gmail.com>
2022-10-15 12:25:58 +0300

    Add project files.

---
+++ Data/Szilveszter_Levente_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Szilveszter_Levente_Lab2.Models;
+
+namespace Szilveszter_Levente_Lab2.Data
+{
+    public class Szilveszter_Levente_Lab2Context : DbContext
+    {
+        public Szilveszter_Levente_Lab2Context (DbContextOptions<Szilveszter_Levente_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Szilveszter_Levente_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Szilveszter_Levente_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Szilveszter_Levente_Lab2.Models.Category> Category { get; set; }
+    }
+}
