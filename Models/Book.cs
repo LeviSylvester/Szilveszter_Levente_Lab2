da21dbdc6017471eb5f744505fddb3dda869cd86
szilveszter.levente@gmail.com <szilveszter.levente@gmail.com>
2022-12-18 14:13:32 +0200

    Laborator3

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,11 +10,20 @@ namespace Szilveszter_Levente_Lab2.Models
     {
         public int ID { get; set; }
 
+        [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuies"), Required, StringLength(50, MinimumLength = 3)]
+        //^ marcheaza inceputul sirului de caractere
+        //[A-Z][a-z]+ prenumele -litera mare urmata de oricate litere mici
+        //\s spatiu
+        //[A-Z][a-z]+ numele -litera mare urmata de oricate litere mici
+        //$ marcheaza sfarsitul sirului de caractere
         public string Author { get; set; }
 
-        [Column(TypeName = "decimal(6, 2)")]
+        [Range(1, 300)]
+        [Column(TypeName = "decimal(6, 2)")]        
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Szilveszter_Levente_Lab2.Data;
 using Szilveszter_Levente_Lab2.Models;
 
 namespace Szilveszter_Levente_Lab2.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Szilveszter_Levente_Lab2.Data.Szilveszter_Levente_Lab2Context _context;
 
@@ -21,7 +15,13 @@ namespace Szilveszter_Levente_Lab2.Pages.Books
 
         public IActionResult OnGet()
         {
-            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            ViewData["PublisherID"] = new SelectList(_context.Publisher, "ID", "PublisherName");
+
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+
+            PopulateAssignedCategoryData(_context, book);
+
             return Page();
         }
 
@@ -30,17 +30,34 @@ namespace Szilveszter_Levente_Lab2.Pages.Books
 
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            if (!ModelState.IsValid)
+            var newBook = new Book();
+            if (selectedCategories != null)
             {
-                return Page();
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
             }
 
-            _context.Book.Add(Book);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            if (await TryUpdateModelAsync<Book>(
+                newBook,
+                "Book",
+                i => i.Title, i => i.Author,
+                 i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                _context.Book.Add(newBook);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            PopulateAssignedCategoryData(_context, newBook);
+            return Page();
         }
     }
 }
